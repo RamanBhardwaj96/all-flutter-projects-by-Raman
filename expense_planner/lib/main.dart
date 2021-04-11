71d27c50dde94bb61f334501a9a5e6f0eb97ecc0
RamanBhardwaj96 <53423311+RamanBhardwaj96@users.noreply.github.com>
2021-04-11 15:14:03 +0530

    init app created

---
+++ expense_planner/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Expense-Planner"),
+      ),
+    );
+  }
+}
